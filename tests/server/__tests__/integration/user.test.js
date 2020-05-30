ece6ae1dcaea51e7cff95b6bb22c61202d105f99
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-30 18:02:52 -0300

    Truncate para Multithread de testes

--- tests/server/__tests__/integration/user.test.js
+++ tests/server/__tests__/integration/user.test.js
@@ -1,8 +1,14 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../utils/truncate';
+
 describe('User', () => {
-  it('Should be able to register', async () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('Should be able to register.', async () => {
     const response = await request(app)
       .post('/users')
       .send({
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should not be able to register with duplicated email.', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Joan Pedro Oliveira de Souza',
+        email: 'joan.pedro@email.com',
+        password_hash: '1r12rjojf',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Joan Pedro Oliveira de Souza',
+        email: 'joan.pedro@email.com',
+        password_hash: '1r12rjojf',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
